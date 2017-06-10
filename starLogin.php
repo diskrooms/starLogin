cf2d32d342ae108ba26d92fa982f2363a25111ae
unknown <ekuaibao1@163.com>
2017-06-11 00:14:55 +0800

    约辽碌

--- starLogin.php
+++ starLogin.php
@@ -11,40 +11,112 @@ class starLogin{
 		$this->weChatAppSecret = $appSecret;
 	}
 	
-	//公众号登录
-	public function weChatOALogin($callback='',$scope = 0){
+	//微信公众号登录
+	public function weChatOALogin($scope = 0){
 		//dump($_SERVER);
 		$curUri = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];	//当前访问地址(不带参数)
 		$code = addslashes(trim($_GET['code']));
+		$callback = addslashes(trim($_GET['callback'])) ? addslashes(trim($_GET['callback'])) : addslashes(trim($_SERVER['HTTP_REFERER']));;
 		if(empty($code)){
-			$callback = empty($callback) ? $_SERVER['HTTP_REFERER'] : $callback;
-			if(empty($callback)){
-				throw new exception('回调地址不能为空');	
-			}
-			$callback = urlencode($callback);
-			$redirectUri = urlencode($curUri.'?callback='.$callback);
+			$redirectUri = urlencode($curUri.'?callback='.urlencode($callback));
 			$scope = ($scope == 0) ? 'snsapi_base' : 'snsapi_userinfo';
 			$authorizeUrl = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid='.$this->weChatAppId.'&redirect_uri='.$redirectUri.'&response_type=code&scope='.$scope.'&state=isWXAddr&connect_redirect=1#wechat_redirect';	
 			header('Location:'.$authorizeUrl);
 			exit();
 		} else {
-			$callback = addslashes(trim($_GET['callback']));
-			if(empty($callback)){
-				throw new exception('没有接收到回调地址');	
-			}
-			$access_token_json = file_get_contents('https://api.weixin.qq.com/sns/oauth2/access_token?appid='.$this->weChatAppId.'&secret='.$this->weChatAppSecret.'&code='.$code.'&grant_type=authorization_code');
-
+			$access_token_json = $this->requestGet('https://api.weixin.qq.com/sns/oauth2/access_token?appid='.$this->weChatAppId.'&secret='.$this->weChatAppSecret.'&code='.$code.'&grant_type=authorization_code');
 			$access_token_arr = json_decode($access_token_json,true);
 			$access_token = $access_token_arr['access_token'];
 			$openid = $access_token_arr['openid'];
+			
 			//读取用户信息
-			$userinfo_json = file_get_contents('https://api.weixin.qq.com/sns/userinfo?access_token='.$access_token.'&openid='.$openid.'&lang=zh_CN');
+			$userinfo_json = $this->requestGet('https://api.weixin.qq.com/sns/userinfo?access_token='.$access_token.'&openid='.$openid.'&lang=zh_CN');
             $userinfo_arr = json_decode($userinfo_json,true);
-			header('Location:'.$callback);
+			
 			return $userinfo_arr;
+			
 		}
 	}
 	
-	//二维码扫描登录
+	//微信PC网站扫码登录
+	public function weChatQRLogin(){
+		$curUrl = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];	//当前访问地址(不带参数)
+		$code = addslashes(trim($_GET['code']));
+		if(empty($code)){
+			$redirectUrl = urlencode($curUrl);
+			$authorizeUrl = 'https://open.weixin.qq.com/connect/qrconnect?appid='.$this->weChatAppId.'&redirect_uri='.$redirectUrl.'&response_type=code&scope=snsapi_login&state=isWXAddr#wechat_redirect';	
+			header('Location:'.$authorizeUrl);
+			exit();
+		} else {
+			$access_token_json = $this->requestGet('https://api.weixin.qq.com/sns/oauth2/access_token?appid='.$this->weChatAppId.'&secret='.$this->weChatAppSecret.'&code='.$code.'&grant_type=authorization_code');
+			$access_token_arr = json_decode($access_token_json,true);
+			return $access_token_arr;
+		}
+	}
 	
+	
+	
+	public function callback($callback=''){
+		if(empty($callback)){
+			$callback = addslashes(trim($_GET['callback'])) ? addslashes(trim($_GET['callback'])) : addslashes(trim($_SERVER['HTTP_REFERER']));
+		}
+		if(empty($callback)){
+			throw new exception('回调地址不能为空');
+		}
+		header('Location:'.$callback);
+	}
+	
+	//GET请求数据 如果有curl扩展 就使用curl进行请求 如果没有相应模块 就使用file_get_contents函数
+	private function requestGet($url='',$timeout=6){
+		if(empty($url)){
+			throw new exception('url参数不能为空');
+		}
+		$ch = curl_init();
+		if($ch){
+			//设置curl
+			curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
+			curl_setopt($ch, CURLOPT_URL, $url);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER,FALSE); //不认证证书
+			curl_setopt($ch, CURLOPT_SSL_VERIFYHOST,FALSE);
+			curl_setopt($ch, CURLOPT_HEADER, FALSE);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+			$content = curl_exec($ch);
+	        curl_close($ch);
+		} else {
+			$content = file_get_contents($url);
+		}
+		return $content;
+	}
+	
+	//POST请求数据 如果有curl扩展 就使用curl进行请求 如果没有相应模块 就是用file_get_contents函数
+	private function requestPost($url='',$data=array(),$timeout=6){
+		if(empty($url)){
+			throw new exception('url参数不能为空');
+		}
+		$ch = curl_init();
+		if($ch){
+			//设置curl
+			curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
+			curl_setopt($ch, CURLOPT_URL, $url);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER,FALSE); //不认证证书
+			curl_setopt($ch, CURLOPT_SSL_VERIFYHOST,FALSE);
+			curl_setopt($ch, CURLOPT_HEADER, FALSE);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+			curl_setopt($ch, CURLOPT_POST, 1);				// post方式
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $data);	// post数据
+			$content = curl_exec($ch);
+	        curl_close($ch);
+		} else {
+			$data = http_build_query($data);
+			$context = array(
+				'http'=>array(
+					'method'=>'POST',
+					'content'=>$data
+				)
+			);
+			$context  = stream_context_create($context);
+			$content = file_get_contents($url,false,$context);
+		}
+		return $content;
+	}
 }
--- thinkphp3.2.3/Application/Home/Controller/TestTPController.class.php
+++ thinkphp3.2.3/Application/Home/Controller/TestTPController.class.php
@@ -2,7 +2,7 @@
 /**
  * @Author diskrooms
  **/
-namespace Home\Controller;
+namespace User\Controller;
 
 use Think\Controller;
 //api首页
@@ -13,7 +13,7 @@ class TestTPController extends Controller {
 		Vendor('starLogin.starLogin');
 	}
 	
-	//微信公众号登录
+	//微信公众号登录接口
 	public function testWeChatOALogin(){
 		
 		//dump($_SERVER);
@@ -21,12 +21,45 @@ class TestTPController extends Controller {
 			echo '登录成功';	
 		} else {
 			$login = new \starLogin('','');
-			$userInfo = $login->weChatOALogin('http://www.yaoyaoyouxi.com/',1);
+			$userInfo = $login->weChatOALogin(1);
 			$_SESSION['uid'] = $userInfo['openid'];
-			
+			//处理业务逻辑
+			$login->callback();
 		}
 	}
 	
+	//微信PC网站扫码登录
+	public function testWeChatQRLogin(){
+		if($_SESSION['uid']){
+			echo '登录成功';	
+			
+		} else {
+			$login = new \starLogin('','');
+			$userInfo = $login->weChatQRLogin();
+			//dump($userInfo);
+			//exit();
+			$_SESSION['uid'] = $userInfo['openid'];
+			//处理业务逻辑
+			
+			$login->callback('/testTP/testWeChatQRLoginIndex');
+		}
+		
+	}
 	
+	public function testWeChatOALoginIndex(){
+		
+		$uid = $_SESSION['uid'] ? $_SESSION['uid'] : 0;
+		
+		$this->assign('uid',$uid);
+		$this->display();
+	}
+	
+	public function testWeChatQRLoginIndex(){
+		
+		$uid = $_SESSION['uid'] ? $_SESSION['uid'] : 0;
+		
+		$this->assign('uid',$uid);
+		$this->display();
+	}
 	
 }
